77119f1332bfa264526ca721e959c34f9627d814
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:30:42 +0500

    Qno09 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno09 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno9.js
@@ -0,0 +1,5 @@
+"use strict";
+/* Favourite Number: Store your favourite number in a variable. Then, using that variable
+ create a message that reveals your favourite number. Print that message.*/
+let favNum = 1598;
+console.log(`My Favourite Number is ${favNum}`);
